bec4e4e46aff4ef07eb124963b4be1491de2e140
Crieve <dylanfortune@live.com>
2023-08-20 19:40:59 -0700

    Added a sound pack example using only world of warcraft sound files.

--- ATT-SoundPack-Template.lua
+++ ATT-SoundPack-Template.lua
@@ -42,6 +42,7 @@ api:CreateSoundPack(appName, {
 -- This makes it extremely unlikely that you'll encounter a naming collision.
 api:CreateSoundPack("Age of Empires (" .. appName .. ")", {
 	COMPLETE = {
+		-- NOTE: An asset can be an asset path (like the function call), an explicit string path, or a number representing the built-in audio sound you want to play.
 		asset("aoe/complete1.mp3"),
 	},
 	DEATH = {
@@ -79,5 +80,47 @@ api:CreateSoundPack("Final Fantasy VII (" .. appName .. ")", {
 	},
 	REPORT = {
 		asset("ffvii/report1.mp3"),
+	},
+});
+
+-- This example shows audio file path ids.
+-- You can look them up on WoWHead. Note that if you are targetting Classic,
+-- you should make sure that the audio was available at the time!
+api:CreateSoundPack("WoW Sound Effects (" .. appName .. ")", {
+	COMPLETE = {
+		8571,	-- https://www.wowhead.com/classic/sound=8571/crowdcheeralliance1
+		8572,	-- https://www.wowhead.com/classic/sound=8572/crowdcheeralliance2
+		8573,	-- https://www.wowhead.com/classic/sound=8574/crowdcheerhorde1
+		8574,	-- https://www.wowhead.com/classic/sound=8574/crowdcheerhorde2
+	},
+	DEATH = {
+		8860,	-- https://www.wowhead.com/classic/sound=8860/a-raz-naxx-death
+	},
+	FANFARE = {
+		2737,	-- https://www.wowhead.com/classic/sound=2737/dwarf-female-vocal-09-cheer
+		2725,	-- https://www.wowhead.com/classic/sound=2725/dwarf-male-vocal-09-cheer
+		2689,	-- https://www.wowhead.com/classic/sound=2689/human-female-vocal-09-cheer
+		2677,	-- https://www.wowhead.com/classic/sound=2677/human-male-vocal-09-cheer
+		2847,	-- https://www.wowhead.com/classic/sound=2847/gnome-female-vocal-09-cheer
+		2835,	-- https://www.wowhead.com/classic/sound=2835/gnome-male-vocal-09-cheer
+		2761,	-- https://www.wowhead.com/classic/sound=2761/nightelf-female-vocal-09-cheer
+		2749,	-- https://www.wowhead.com/classic/sound=2749/nightelf-male-vocal-09-cheer
+		2713,	-- https://www.wowhead.com/classic/sound=2713/orc-female-vocal-09-cheer
+		2701,	-- https://www.wowhead.com/classic/sound=2701/orc-male-vocal-09-cheer
+		2810,	-- https://www.wowhead.com/classic/sound=2810/tauren-female-vocal-09-cheer
+		2797,	-- https://www.wowhead.com/classic/sound=2797/tauren-male-vocal-09-cheer
+		2871,	-- https://www.wowhead.com/classic/sound=2871/troll-female-vocal-09-cheer
+		2859,	-- https://www.wowhead.com/classic/sound=2859/troll-male-vocal-09-cheer
+		2785,	-- https://www.wowhead.com/classic/sound=2785/undead-female-vocal-09-cheer
+		2773,	-- https://www.wowhead.com/classic/sound=2773/undead-male-vocal-09-cheer
+	},
+	RAREFIND = {
+		3439,	-- https://www.wowhead.com/classic/sound=3439/horngoober
+	},
+	REMOVE = {
+		8693,	-- https://www.wowhead.com/classic/sound=8693/valentines-brokenheart
+	},
+	REPORT = {
+		8694,	-- https://www.wowhead.com/classic/sound=8694/valentines-lookingforloveheart
 	},
 });
